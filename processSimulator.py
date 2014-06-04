8e67311ea83eba3fb8a8c0e8ea613c5128959d97
Jonas Amaro <jonasrla@gmail.com>
2014-06-04 00:30:14 -0300

    begin the definition of process and robinRound classes

---
+++ processSimulator.py
@@ -0,0 +1,18 @@
+class process:
+	def __init__(self, pid, time, cpuBurst):
+		self.pid = pid
+		self.time = time
+		self.cpuBurst = cpuBurst
+	def work(tempo):
+		if tempo < self.time:
+			self.time -=tempo
+			return False
+		else:
+			return True
+
+
+
+class roundRobin:
+	def __init__(self, quantum):
+		self.line = []
+		self.quantum = quantum

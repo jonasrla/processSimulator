2492d63118088661e81b15fb10cb6be085658480
Jonas Amaro <jonasrla@gmail.com>
2014-06-05 11:22:27 -0300

    creates class schedule that have 3 queues as specified

--- processSimulator.py
+++ processSimulator.py
@@ -16,6 +16,15 @@ class roundRobin:
 	def __init__(self, quantum):
 		self.line = []
 		self.quantum = quantum
+
+
 class fcfs:
 	def __init__(self):
 		self.line = []
+
+
+class schedule:
+	def __init__(self):
+		self.q0 = roundRobin(10)
+		self.q1 = roundRobin(20)
+		self.q2 = fcfs()

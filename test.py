e443b9b47776fddfb07adc867be7e0539f911030
Jan Segre <jan@segre.in>
2014-06-06 14:02:00 -0300

    Better format for tests.

--- test.py
+++ test.py
@@ -3,13 +3,9 @@ from processSimulator import Process
 
 queues = [
     [Process(1, burst=80, ios=1)],
-    [Process(1, burst=80, ios=1), Process(2, 40), Process(3, 20, 4)],
+    [Process(1, burst=80, ios=1), Process(2, burst=40), Process(3, burst=20, ios=4)],
     [Process(1, burst=10), Process(2, burst=5, ios=4), Process(3, burst=30, ios=1)],
 ]
-
-# 150 2
-# {'burst': 150, 'IO': 2}
-# {'time': 50, 'kind': 'CPU'}, {'time': 20, 'kind': 'IO'}, {'time': 50, 'kind': 'CPU'}, {'time': 20, 'kind': 'IO'}, {'time': 50, 'kind': 'CPU'}
 
 for i, queue in enumerate(queues):
     print '# Example', i + 1
